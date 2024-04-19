b8920cc2c2d8240b0e09f31b80563b303a913528
Nikhil Barthwal <nikhil.barthwal@sentry.io>
2024-04-18 23:53:34 -0700

    inverse

--- fft.py
+++ fft.py
@@ -1,11 +1,10 @@
 import numpy as np
-import cmath
-import math
 import random
 from typing import List
+import math
 
 
-def equal(a, b):
+def test(a, b):
     if len(a) != len(b):
         return False
     for i in range(len(a)):
@@ -14,58 +13,50 @@ def equal(a, b):
     return True
 
 
-def ifft(x):
-    N = len(x)
-    if N <= 1:
-        return x
-    else:
-        even = ifft(x[::2])
-        odd = ifft(x[1::2])
-        factor = np.exp(2j * np.pi * np.arange(N) / N)
-        return np.concatenate([even + factor[:N//2] * odd,
-                               even + factor[N//2:] * odd]) / 2
-
-
 class FFT:
 
     def __init__(self, indexes: List[int], inverse: bool):
+        self.k = 2 if inverse else 1
         self.indexes: List[int] = indexes
         self.N: int = len(indexes)
         if self.N > 1:
             assert self.N % 2 == 0
-            self.even = FFT(indexes[::2])
-            self.odd = FFT(indexes[1::2])
+            self.even = FFT(
+                [indexes[i] for i in range(self.N) if i % 2 == 0], inverse)
+            self.odd = FFT(
+                [indexes[i] for i in range(self.N) if i % 2 == 1], inverse)
             self.factor = []
             for i in range(self.N):
-                self.factor.append(np.exp(-2j * np.pi * i / self.N))
+                k = 1 if inverse else -1
+                theta = k * 2 * np.pi * i / self.N
+                self.factor.append(math.cos(theta) + 1j * math.sin(theta))
 
-    def fft(self, x):
-        if self.N <= 1:
+    def compute(self, x):
+        assert self.N > 0
+        if self.N == 1:
             return [x[self.indexes[0]]]
 
         assert self.N % 2 == 0
-        even = self.even.fft(x)
-        odd = self.odd.fft(x)
+        even = self.even.compute(x)
+        odd = self.odd.compute(x)
         half = self.N // 2
         assert len(even) == half
         assert len(odd) == half
-        r1 = []
-        r2 = []
+        r = [0 + 0j] * self.N
         for i in range(half):
-            r1.append(even[i] + self.factor[i] * odd[i])
-            r2.append(even[i] + self.factor[half + i] * odd[i])
-        return r1 + r2
+            r[i] = (even[i] + self.factor[i] * odd[i]) / self.k
+            r[half + i] = (even[i] + self.factor[half + i] * odd[i]) / self.k
+        return r
 
 
-def main(n):
+def run(n):
     indexes = list(range(2**n))
-    f = FFT(indexes)
+    f = FFT(indexes, False)
+    inv_f = FFT(indexes, True)
     x = [random.randint(0, 1000) for _ in indexes]
-    y = f.fft(x)
-    z = np.fft.fft(x)
-    assert equal(y, z)
-    xx = ifft(y)
-    assert equal(x, xx)
+    y = f.compute(x)
+    assert test(y, np.fft.fft(x))
+    assert test(x, inv_f.compute(y))
 
 
-main(6)
+run(10)
